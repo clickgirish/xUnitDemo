53edbf6b42dd4e690367f6d09338e1ebb9145256
Girish Chawrai <clickgirish@gmail.com>
2024-09-04 08:31:25 +0530

    Added a console app for executing business functions.

---
+++ Business/StaticParamLogic.cs
@@ -0,0 +1,17 @@
+﻿using Business.Data;
+using Business.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business
+{
+    public class StaticParamLogic
+    {
+        public StaticParam? GetParamById(string key)
+        {
+            MangoProductAPIContext context = new MangoProductAPIContext();
+            var staticParam = context.StaticParams.FirstOrDefault(p => p.Key == key);
+
+            return staticParam;
+        }
+    }
+}
---
+++ BusinessExecutor/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessExecutor
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            StaticParamLogic logic = new StaticParamLogic();
+            var staticParam = logic.GetParamById("Param1");
+
+            if (staticParam != null)
+                Console.WriteLine(staticParam.Value);
+            else
+                Console.WriteLine(" Static Param not found");
+
+            Console.ReadLine();
+        }
+    }
+}
